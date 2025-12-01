0ee82155e33bb9e23193f7e1b2e6c30159c49cd1
OMGiTzPomPom <pauline.srifi@gmail.com>
2025-12-01 14:14:29 +0100

    day1 start

---
+++ src/_2025/day1/Day1.java
@@ -0,0 +1,15 @@
+package _2025.day1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import _2023.InputReader;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+        ArrayList<String> list = (ArrayList<String>) InputReader.readFile(1);
+        System.out.println(list);
+    }
+}
