653fae212f3dee474a772a00d034665f054bed5b
OMGiTzPomPom <pauline.srifi@gmail.com>
2025-12-03 15:03:58 +0100

    day3 ex2 ok

--- src/_2025/day3/Day3.java
+++ src/_2025/day3/Day3.java
@@ -11,7 +11,7 @@
         public static void main(String[] args) {
             ArrayList<String> list = (ArrayList<String>) InputReader.readFile(3);
             System.out.println(levelOne(list));
-//            System.out.println(levelTwo(list));
+            System.out.println(levelTwo(list));
         }
 
         private static int levelOne(List<String> list) {
@@ -39,11 +39,50 @@
                     total += best;
             }
             return total;
-        } 
+        }
 
+        public static long levelTwo(List<String> list) {
+            long total = 0L;
+            for (String s : list) {
+                long best = maxKDigitsValue(s, 12);
+                total += best;
+            }
+            return total;
+        }
 
-        private static int levelTwo(List<String> list) {
-            System.out.print("Level Two: ");
-            return 0;
+        private static long maxKDigitsValue(String s, int k) {
+            StringBuilder digits = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (Character.isDigit(c)) digits.append(c);
+            }
+
+            int n = digits.length();
+            if (n < k) { //circuit breaker
+                return 0L;
+            }
+
+            int toRemove = n - k;
+            StringBuilder stack = new StringBuilder();
+
+            for (int i = 0; i < n; i++) {
+                char c = digits.charAt(i);
+                while (stack.length() > 0 && toRemove > 0 && stack.charAt(stack.length() - 1) < c) {
+                    stack.deleteCharAt(stack.length() - 1);
+                    toRemove--;
+                }
+                stack.append(c);
+            }
+
+            if (toRemove > 0) {
+                stack.setLength(stack.length() - toRemove);
+            }
+            String resultDigits = stack.substring(0, k);
+
+            long value = 0L;
+            for (int i = 0; i < resultDigits.length(); i++) {
+                value = value * 10L + (resultDigits.charAt(i) - '0');
+            }
+            return value;
         }
     }
